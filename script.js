2553086966a61aa70f6ca3f5117b0c89255a3b7c
Carlos Eduardo <cpwaldow@gmail.com>
2023-05-30 09:43:07 -0300

    adicionando validação para o botão

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const btn = document.querySelector('button');
 const h2 = document.getElementById('text');
 const textArea = document.querySelector('textarea');
 const container = document.querySelector('.text-container');
+
+textArea.addEventListener('input', () => {
+  if (textArea.value.length > 0) {
+    btn.disabled = false;
+  } else {
+    btn.disabled = true;
+  }
+});
+
 btn.addEventListener('click', () => {
   h2.innerText = textArea.value.toUpperCase();
   textArea.value = '';
